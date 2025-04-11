34418c331f058f93fc60831b76fb8637ecd9aab3
Hamza Kamran <hamzakamrn8@gmail.com>
2025-04-11 15:09:08 +0500

    header setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const navigate = [
+    {
+      label: "Tv Shows",
+      href: "tv",
+    },
+    {
+      label: "Movies",
+      href: "movie",
+    },
+  ];
+
   return (
-    <header className="fixed top-0 w-full h-16 bg-neutral-600 opacity-75">
-      <div className="container mx-auto px-2">Header</div>
+    <header className="fixed top-0 w-full h-16 bg-neutral-600  bg-opacity-75">
+      <div className="container mx-auto px-3 flex items-center h-full">
+        <div>
+          <img src={logo} alt="logo" width={120} />
+        </div>
+        <nav className="flex items-center gap-1 ml-5">
+          {navigate.map((nav, index) => {
+            return (
+              <div>
+                <NavLink key={nav.label} to={nav.href} className={"px-2 hover:text-neutral-100"}>
+                {nav.label}
+                </NavLink>
+              </div>
+            );
+          })}
+        </nav>
+      </div>
     </header>
   );
 };
