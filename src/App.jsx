86b1dbb068281e62a67636af4e6f4c09e1626543
Hamza Kamran <hamzakamrn8@gmail.com>
2025-04-15 17:49:54 +0500

    redux store and banner sec setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,39 @@ import Footer from "./components/Footer";
 import MobileNavigation from "./components/MobileNavigation";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { setBannerData } from "./store/movieSlice";
+import { setBannerData, setImageURL } from "./store/movieSlice";
 
 function App() {
   const Dispatch = useDispatch();
 
   const FetchTrendingData = async () => {
     try {
-      const response = await axios.get("trending/all/week");
+      const response = await axios.get("/trending/all/week");
       Dispatch(setBannerData(response.data.results));
     } catch (error) {
       console.log("error", error);
     }
   };
+
+  const FetchConfiguration = async () => {
+    try {
+      const response = await axios.get("/configuration");
+      Dispatch(setImageURL(response.data.images.secure_base_url + "original"));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   useEffect(() => {
     FetchTrendingData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    FetchConfiguration();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <main className="pb-14 lg:pb-0">
       <Header />
-      <div className="pt-16">
+      <div className="">
         <Outlet />
       </div>
       <Footer />
--- src/components/BannerHome.jsx
+++ src/components/BannerHome.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
 const BannerHome = () => {
-  return (
-    <div>BannerHome</div>
-  )
-}
+  const BannerData = useSelector((state) => state.movieoData.BannerData);
+  const imageURL = useSelector((state) => state.movieoData.imageURL);
+  const [currentImage, setCurrentImage] = useState(0);
 
-export default BannerHome
+  const handleNext = () => {
+    if (currentImage < BannerData.length - 1) {
+      setCurrentImage((prev) => prev + 1);
+    }
+  };
+  const handlePrevious = () => {
+    if (currentImage > 0) {
+      setCurrentImage((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <div className="w-full h-full">
+      <div className="flex min-h-full max-h-[95vh] overflow-hidden">
+        {BannerData.map((data, index) => {
+          console.log("data", data);
+          return (
+            <div
+              key={index}
+              className="min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative group transition-all"
+              style={{ transform: `translateX(-${currentImage * 100}%)` }}
+            >
+              <div className="w-full h-full">
+                <img
+                  src={imageURL + data.backdrop_path}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+
+              {/* Button Prev and Next Slides */}
+              <div className="absolute top-0 h-full w-full hidden items-center justify-between px-4 group-hover:lg:flex">
+                <button
+                  onClick={handlePrevious}
+                  className="bg-white p-2 rounded-full text-xl text-black z-10"
+                >
+                  <FaAngleLeft />
+                </button>
+                <button
+                  onClick={handleNext}
+                  className="bg-white p-2 rounded-full text-xl text-black z-10"
+                >
+                  <FaAngleRight />
+                </button>
+              </div>
+
+              <div className="absolute top-0 w-full h-full bg-gradient-to-t from-neutral-900 to-transparent"></div>
+              <div className="container mx-auto">
+                <div className=" w-full absolute bottom-0 max-w-md px-3">
+                  <h2 className="font-bold text-2xl lg:text-4xl text-white drop-shadow-2xl">
+                    {data.name || data.title}
+                  </h2>
+                  <p className="text-ellipsis line-clamp-3 my-2">
+                    {data.overview}
+                  </p>
+                  <div className="flex gap-4">
+                    <p>Rating: {Number(data.vote_average).toFixed(1)}+</p>
+                    <span>|</span>
+                    <p>View: {Number(data.popularity).toFixed(0)}</p>
+                  </div>
+                  <button className="bg-white text-black px-4 py-2 font-bold rounded mt-4 hover:bg-gradient-to-l  from-red-700 to-orange-500 shadow-md transition-all hover:scale-105">
+                    Play Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default BannerHome;
